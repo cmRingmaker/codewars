ac16a1bd92447ebf9f5ee9cebb302ca289b0dc31
ringmaker <chrisringmaker@gmail.com>
2022-08-15 20:19:47 -0400

    added new kata

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,15 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+function solution(str){
+  
+}
+*/
+
+// My Solution:
+function solution(str){
+    return str.split("").reverse().join("")
+}
