fd5fd3c4750a4fffef4ca166b8e60528cdcafbe2
ringmaker <chrisringmaker@gmail.com>
2022-08-17 23:15:09 -0400

    new katas added

---
+++ MakeUpperCase.js
@@ -0,0 +1,11 @@
+/*Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+}
+*/
+
+// My Solution:
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
