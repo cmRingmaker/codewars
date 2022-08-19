62bfb5075730e42a0797211b22fe364ed712cfb0
ringmaker <chrisringmaker@gmail.com>
2022-08-19 00:15:21 -0400

    added more katas

---
+++ Function 3 - multiplying two numbers.js
@@ -0,0 +1,13 @@
+/*Implement a function called multiply, which takes two numbers and returns their product:
+
+multiply(2, 3) = 6
+multiply(0, 188) = 0
+multiply(85, 144) = 12240
+
+// Write here your multiply-function
+*/
+
+// My Solution:
+function multiply(n1, n2) {
+    return n1 * n2
+}
