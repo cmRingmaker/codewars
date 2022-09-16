4d087c9cb19ec1e62e00cdefc5e3eb13650a54de
ringmaker <chrisringmaker@gmail.com>
2022-09-15 20:56:50 -0400

    fake binary kata added

---
+++ 8kyu/Fake Binary.js
@@ -0,0 +1,13 @@
+/*CGiven a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+function fakeBin(x){
+
+}
+*/
+
+// My Solution:
+function fakeBin(x){
+    return x.split('').map(e => e < 5 ? 0 : 1).join('')
+}
