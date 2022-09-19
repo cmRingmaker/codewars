bfe51ce5f8d5f9fbb4d90dd4b6d77d4ea2f11813
ringmaker <chrisringmaker@gmail.com>
2022-09-18 23:20:27 -0400

    calculate average kata added

---
+++ 8kyu/Calculate average.js
@@ -0,0 +1,14 @@
+/*Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  // your code here
+  return 0;
+}
+*/
+
+// My Solution:
+function find_average(array) {
+    return array.length === 0 ? 0 : array.reduce((acc, c) => acc + c, 0) / array.length
+}
