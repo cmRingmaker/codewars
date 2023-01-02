35c33db259dfc7114c715292a0c302089d2d098c
ringmaker <chrisringmaker@gmail.com>
2023-01-02 15:53:50 -0500

    unfinished loop - bug fix

---
+++ 8kyu/Unfinished Loop - Bug Fixing #1.js
@@ -0,0 +1,23 @@
+/*Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number;){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
+*/
+
+// My Solution:
+function createArray(number) {
+    var newArray = [];
+    
+    for(let counter = 1; counter <= number; counter++){
+        newArray.push(counter);
+    }
+    
+    return newArray;
+}
