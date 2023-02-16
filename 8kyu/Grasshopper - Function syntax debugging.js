1712423794e2cb4b7b4ffe793a54f2ddcdfff0a9
ringmaker <chrisringmaker@gmail.com>
2023-02-15 23:50:06 -0500

    grasshopper function syntax debugging

---
+++ 8kyu/Grasshopper - Function syntax debugging.js
@@ -0,0 +1,10 @@
+/*A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main [verb, noun]
+  return verb + noun
+}
+*/
+
+
+// My Solution:
+const main = (a, b) => a + b
