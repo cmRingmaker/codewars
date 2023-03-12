5cad899edb547f61654e3f0747a7437922585c26
ringmaker <chrisringmaker@gmail.com>
2023-03-11 23:14:30 -0500

    variable assignment debug

---
+++ 8kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,16 @@
+/*Variable assignment
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a == "dev"
+var b == "Lab"
+
+var name == a + b
+*/
+
+
+// My Solution:
+const a = "dev"
+const b = "Lab"
+
+const name = a + b
