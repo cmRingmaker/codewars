6085821b606084c78e23de90725b7fd11aaf4bad
ringmaker <chrisringmaker@gmail.com>
2023-09-11 23:53:15 -0400

    maximum length difference

---
+++ 7kyu/Maximum Length Difference.js
@@ -0,0 +1,35 @@
+/*You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+Example:
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+
+function mxdiflg(a1, a2) {
+    // your code
+}
+*/
+
+
+// My Solution:
+
+// -----------------------------------------------------------------
+// this is literally the worst Kata description I have ever seen
+// what it actually wants you to do is this:
+
+// Find maximum and minimum in both arrays
+// Compare maximum from array1 with minimum of array2
+// Compare maximum from array2 with minimum of array1
+// Return the maximum from botrh comparisions
+// -----------------------------------------------------------------
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let l1 = a1.map(str => str.length)
+  let l2 = a2.map(str => str.length)
+  return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
