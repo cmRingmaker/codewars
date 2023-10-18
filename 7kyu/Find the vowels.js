affa317e3a07b0247b81839c8de09a4558b40484
ringmaker <chrisringmaker@gmail.com>
2023-10-17 22:23:32 -0400

    find the vowels

---
+++ 7kyu/Find the vowels.js
@@ -0,0 +1,35 @@
+/*We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+
+NOTES
+
+    Vowels in this context refers to: a e i o u y (including upper case)
+    This is indexed from [1..n] (not zero indexed!)
+
+function vowelIndices(word){
+  //your code here
+}
+*/
+
+
+// My Solution:
+function vowelIndices(word){
+  const letters = word.toLowerCase()
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+  const arr = []
+  
+  for(let i = 0; i < letters.length; i++) {
+    if(vowels.includes(letters[i])) {
+      arr.push(i + 1)
+    }
+  }
+  
+  return arr
+}
