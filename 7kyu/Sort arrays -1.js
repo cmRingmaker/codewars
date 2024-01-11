9a6f860f875ea82864e628548d678d345922affe
ringmaker <chrisringmaker@gmail.com>
2024-01-10 23:06:38 -0500

    sory arrays -1

---
+++ 7kyu/Sort arrays -1.js
@@ -0,0 +1,12 @@
+/*Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+}
+*/
+
+
+// My Solution:
+
+const sortme = names => names.sort()
