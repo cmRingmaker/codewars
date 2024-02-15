644faa97236888580d3507ff90bd11c728e8bacb
ringmaker <chrisringmaker@gmail.com>
2024-02-14 23:38:02 -0500

    digits explosion

---
+++ 7kyu/Digits explosion.js
@@ -0,0 +1,16 @@
+/*Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+Examples
+
+"312" should return "333122"
+
+"102269" should return "12222666666999999999"
+
+function explode(s) {
+  return "";
+}
+*/
+
+
+// My Solution:
+
+const explode = (s) => s.split('').map((n) => n.repeat(n)).join('')
