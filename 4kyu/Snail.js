ffb9f92fbdc087fbdd5fdc76878513222b64742b
ringmaker <chrisringmaker@gmail.com>
2024-04-08 23:59:55 -0400

    snail

---
+++ 4kyu/Snail.js
@@ -0,0 +1,37 @@
+/*Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+snail = function(array) {
+  // enjoy
+}
+*/
+
+
+// My Solution:
+
+const snail = (array) =>{
+  let finalArray = []
+
+  while(array.length){
+    finalArray.push(...array.shift())
+    for (var i = 0; i < array.length; i++){
+      finalArray.push(array[i].pop())
+    }
+    finalArray.push(...(array.pop() || []).reverse())
+    for (var i = array.length -1; i >= 0; i--){
+      finalArray.push(array[i].shift())
+    }
+  }
+  return finalArray
+}
